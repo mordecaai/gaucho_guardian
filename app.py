767860b4fb55f5aa8eb1502426b9e2e5cf6f9612
Landon Gemmill <landon@gemmill.net>
2026-01-10 15:03:31 -0800

    redid ui

--- app.py
+++ app.py
@@ -1,28 +1,7 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    output = ""
-    if request.method == "POST":
-        input1 = request.form.get("input1", "")
-        input2 = request.form.get("input2", "")
-        output = f"Input 1: {input1}, Input 2: {input2}"
-    
-    return render_template_string("""
-    <!DOCTYPE html>
-    <html>
-    <head><title>Gaucho Guardian</title></head>
-    <body>
-        <form method="POST">
-            <input type="text" name="input1" placeholder="Input 1"><br><br>
-            <input type="text" name="input2" placeholder="Input 2"><br><br>
-            <button type="submit">Submit</button>
-        </form>
-        {% if output %}
-        <p><strong>Output:</strong> {{ output }}</p>
-        {% endif %}
-    </body>
-    </html>
-    """, output=output)
+    return render_template("index.html")
